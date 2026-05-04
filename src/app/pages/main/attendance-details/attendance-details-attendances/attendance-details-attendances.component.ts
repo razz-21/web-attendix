5ba9072b7649b873d2ca7790c57fc89bf850b503
Ernesto Razo <razz.ernesto@gmail.com>
2026-05-04 21:25:57 +0800

    feat(attendances): update attendance details layout and styling, add 'Add Attendance Record' button

--- src/app/pages/main/attendance-details/attendance-details-attendances/attendance-details-attendances.component.ts
+++ src/app/pages/main/attendance-details/attendance-details-attendances/attendance-details-attendances.component.ts
@@ -1,9 +1,15 @@
 import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-attendance-details-attendances',
   templateUrl: './attendance-details-attendances.component.html',
   styleUrls: ['./attendance-details-attendances.component.scss'],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+  ]
 })
 export class AttendanceDetailsAttendancesComponent {
 }
