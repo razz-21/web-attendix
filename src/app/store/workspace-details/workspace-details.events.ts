5b8968fb8363eea2bad3e483f05f1605a83a2629
AshtonLactuan <anlactuan93824@liceo.edu.ph>
2026-05-17 13:40:42 +0800

    feat(workspace): display groups in workspace

--- src/app/store/workspace-details/workspace-details.events.ts
+++ src/app/store/workspace-details/workspace-details.events.ts
@@ -2,6 +2,7 @@ import { GetPaginatedUsers, GetUser, User } from "@/app/types/users/users.type";
 import { GetWorkspace, PatchWorkspace } from "@/app/types/workspaces/workspaces.types";
 import { type } from "@ngrx/signals";
 import { eventGroup } from "@ngrx/signals/events";
+import { GetGroup } from "@/app/types/groups/groups.type";
 
 export const WorkspaceDetailsEvents = eventGroup({
   source: 'Workspace Details',
@@ -13,6 +14,10 @@ export const WorkspaceDetailsEvents = eventGroup({
     loadWorkspaceUsers: type<{ id: string }>(),
     loadWorkspaceUsersSuccess: type<{ users: GetUser[] }>(),
     loadWorkspaceUsersFailure: type<string>(),
+
+    loadWorkspaceGroups: type<{ id: string }>(),
+    loadWorkspaceGroupsSuccess: type<{ groups: GetGroup[] }>(),
+    loadWorkspaceGroupsFailure: type<string>(),
 
     removeWorkspaceUser: type<{ user: GetUser }>(),
     removeWorkspaceUserSuccess: type<{ userId: string }>(),
